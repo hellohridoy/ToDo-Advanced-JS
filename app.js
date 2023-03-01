8b129d0007f81703ab3163fc4c95527fbceac999
Hridoy Hossain <ridoy.java@gmail.com>
2023-03-02 00:14:28 +0600

    Adding function added

---
+++ app.js
@@ -0,0 +1,50 @@
+//Seclector
+
+const todoInput = document.querySelector('.todo-input');
+const todoButto = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listener
+
+todoButto.addEventListener('click',addTodo)
+
+
+//Functions
+function addTodo(event){
+    //console.log("Hello");
+    //prevent from form submitting
+    event.preventDefault();
+    //todo div create
+    const todoDiv = document.createElement("div");
+    //todo class create
+    todoDiv.classList.add("todo");
+    //create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    //todoInput.value="";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // Check mark Button
+
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+
+
+    // Trash Button
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+
+    todoList.appendChild(todoDiv);
+
+    //Clear input value
+    todoInput.value="";
+
+}
